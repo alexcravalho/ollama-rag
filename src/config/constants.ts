2039b8bde668cdf2ebdcf18fb6c2eacda45f7a89
alexcravalho <a.d.cravalho@gmail.com>
2025-07-05 16:05:30 -0500

    add constant variables and imports

---
+++ src/config/constants.ts
@@ -0,0 +1,5 @@
+import path from 'path';
+export const CHROMA_PATH = 'chroma';
+export const COLLECTION_NAME = 'local-rag';
+export const CHROMA_URL = 'http://localhost:8000';
+export const DATA_PATH = path.resolve(process.cwd(), 'data');
