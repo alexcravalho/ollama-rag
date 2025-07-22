eef36b2e117276c7036e8a712d00638e70f78d06
alexcravalho <a.d.cravalho@gmail.com>
2025-07-22 11:36:41 -0500

    update CHROMA_URL & DATA_PATH; create LITE_LLM_URL

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,5 +1,6 @@
 import path from 'path';
 export const CHROMA_PATH = 'chroma';
 export const COLLECTION_NAME = 'local-rag';
-export const CHROMA_URL = 'http://localhost:8000';
-export const DATA_PATH = path.resolve(process.cwd(), 'data');
+export const CHROMA_URL = 'http://chromadb:8000';
+export const DATA_PATH = path.resolve('/app/data');
+export const LITE_LLM_URL = process.env.LITE_LLM_URL || 'http://litellm-litellm-1:4000';
